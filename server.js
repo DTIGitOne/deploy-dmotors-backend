e6f0e4493b80d1918120dedf99b4fbfa0d59adee
DTIGitOne <149752307+DTIGitOne@users.noreply.github.com>
2024-07-08 13:40:37 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,56 +5,48 @@ import userRouter from './Routers/userRouter.js';
 import carsRouter from './Routers/carsRouter.js';
 import mongoose from 'mongoose';
 import adminRouter from './Routers/AdminRouter.js';
-import http from "http";
+import http from 'http';
 import messageRouter from './Routers/MessageRouter.js';
 
 config();
 
-// MESSAGES //////////////////////////////////////////////////////////
-
+// Create an instance of Express
 const server = express();
 server.use(cors());
+
+// Create an HTTP server
 const serverListen = http.createServer(server);
 
-
-// MIDDLEWARE //////////////////////////////////////////////////////////
-
+// Middleware
 server.use(express.json());
 
-// ROUTERS //////////////////////////////////////////////////////////
-
-server.use('/users' , userRouter);
-
-server.use('/cars' , carsRouter);
-
-server.use('/admin' , adminRouter);
-
+// Routers
+server.use('/users', userRouter);
+server.use('/cars', carsRouter);
+server.use('/admin', adminRouter);
 server.use('/messages', messageRouter);
 
-/////////////////////////////////////////////////////////////////////
+server.get('/Health', (req, res) => {
+  res.status(200).send({ message: 'server is running' });
+});
 
-server.get("/Health" , (req , res) => {
-   res.status(200).send({message: "server is running"});
-})
-
-const port = process.env.SERVER_PORT;
+const port = process.env.PORT || process.env.SERVER_PORT || 3000;
 
 const startServer = () => {
-   serverListen.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
-   });
-}
+  serverListen.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+};
 
 const connectToDatabase = async () => {
-   try {
-      await mongoose.connect(process.env.DATABASE_URL);
-      console.log("Connected to database");
-      startServer();
-   } catch (e) {
-      console.error("There was an issue with connecting to the database");
-      console.error(e);
-   }
-}
-
+  try {
+    await mongoose.connect(process.env.DATABASE_URL);
+    console.log('Connected to database');
+    startServer();
+  } catch (e) {
+    console.error('There was an issue with connecting to the database');
+    console.error(e);
+  }
+};
 
 connectToDatabase();
